b94b2650d89922cf6f42fc936f443138c6384ba7
fulmoon <fulmoon@gmail.com>
2022-08-14 13:21:18 +0900

    first Commit

---
+++ android/app/src/main/kotlin/com/example/currenty_converter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.currenty_converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/currency_converter.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConvert extends StatelessWidget {
+  const CurrencyConvert({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('환율계산기'),
+      ),
+      body: Container(),
+    );
+    ;
+  }
+}
