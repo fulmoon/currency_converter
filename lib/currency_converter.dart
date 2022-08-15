3511acc39f62134306bcdaf811877981a5051ddd
fulmoon <fulmoon@gmail.com>
2022-08-15 19:02:21 +0900

    Feat: add UI for currency app    #Air

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -1,16 +1,157 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConvert extends StatelessWidget {
+class CurrencyConvert extends StatefulWidget {
   const CurrencyConvert({Key? key}) : super(key: key);
+
+  @override
+  State<CurrencyConvert> createState() => _CurrencyConvertState();
+}
+
+class _CurrencyConvertState extends State<CurrencyConvert> {
+  final TextEditingController _controller = TextEditingController();
+
+  List<Map<String, dynamic>> currencyList = [
+    {"USD": 1},
+    {"AED": 3.6725},
+    {"AFN": 89.7035},
+    {"ALL": 113.5506},
+    {"AMD": 405.3498},
+  ];
+
+  String currencyChoose = 'zero';
+  String dropdownValue = 'One';
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('환율계산기'),
+        centerTitle: true,
+        title: const Text(
+          '환율계산기',
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Row(
+                children: [
+                  Container(
+                    width: 200,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 2,
+                          color: Colors.grey,
+                        ),
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(8),
+                          topLeft: Radius.circular(8),
+                        ),
+                    ),
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      dropdownColor: Colors.grey,
+                      value: dropdownValue,
+                      icon: const Icon(Icons.arrow_downward),
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.deepPurple),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: <String>['One', 'Two', 'Free', 'Four']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value, textAlign: TextAlign.center,),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  Flexible(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey, width: 2),
+                          borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(8),
+                            topRight: Radius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: Row(
+                children: [
+                  Container(
+                    width: 200,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.grey,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(8),
+                        topLeft: Radius.circular(8),
+                      ),
+                    ),
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      dropdownColor: Colors.grey,
+                      value: dropdownValue,
+                      icon: const Icon(Icons.arrow_downward),
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.deepPurple),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: <String>['One', 'Two', 'Free', 'Four']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value, textAlign: TextAlign.center,),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  Flexible(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey, width: 2),
+                          borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(8),
+                            topRight: Radius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
-    ;
   }
 }
